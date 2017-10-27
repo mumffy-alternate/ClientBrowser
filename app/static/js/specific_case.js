4200692eb2df9a0e625d3c7610ec432c2732bc42
Mumffy <mumffy@gmail.com>
2017-10-27 00:16:18 -0700

    References to DataTables' CSS and JS.

--- app/static/js/specific_case.js
+++ app/static/js/specific_case.js
@@ -1,6 +1,7 @@
-$(function() {
-    $( "#date_opened" ).datepicker({ dateFormat: 'yy-mm-dd'});
-    $( "#date_closed" ).datepicker({ dateFormat: 'yy-mm-dd'});
+$(document).ready(function() {
+    $("#exampleTable").DataTable();
+    $("#date_opened").datepicker({ dateFormat: 'yy-mm-dd'});
+    $("#date_closed").datepicker({ dateFormat: 'yy-mm-dd'});
     $("#editformtoggle").click(function(){
         var isFormDisabled = $('fieldset').prop('disabled');
         $('fieldset').prop('disabled', !isFormDisabled);
