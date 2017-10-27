fead3971cd633d7c235cdba5358b78af3331aa8a
Mumffy <mumffy@gmail.com>
2017-10-27 02:54:21 -0700

    WIP for DataTable with server-side processing.

--- app/static/js/specific_case.js
+++ app/static/js/specific_case.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-    $("#exampleTable").DataTable();
+    $("#exampleTable").DataTable({
+        "processing":true,
+        "serverSide":true,
+        "ajax":"/api/table/clients"
+    });
     $("#date_opened").datepicker({ dateFormat: 'yy-mm-dd'});
     $("#date_closed").datepicker({ dateFormat: 'yy-mm-dd'});
     $("#editformtoggle").click(function(){
