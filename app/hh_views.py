ba69077f9371c0c07ff01d3c1ea882124c7d14c6
Mumffy <mumffy@gmail.com>
2017-10-30 22:44:05 -0700

    Server-side data retrieval and pagination.

--- app/hh_views.py
+++ app/hh_views.py
@@ -3,13 +3,13 @@ from datetime import datetime
 from flask import render_template, redirect, session, url_for, request, g, request
 from flask_login import login_user, logout_user, current_user, login_required
 
-
 from app import app, db, lm
 from config import POSTS_PER_PAGE
 from .forms import LoginForm, ProfileForm, PostForm
 from .models import Person, Case
 from .hh_forms import ClientForm, CaseForm
 from .hh_utilities import flash
+
 
 @app.route('/hh/clients', methods=['GET', 'POST'])
 def clients():
@@ -34,6 +34,7 @@ def clients():
     clients = Person.query.all()
     return render_template('hh_clients.html', clients=clients, form=form)
 
+
 @app.route('/hh/cases/', methods=['GET', 'POST'])
 def cases(case_name_front=None, case_name_back=None):
     form = CaseForm()
@@ -51,6 +52,7 @@ def cases(case_name_front=None, case_name_back=None):
 
     cases = Case.query.all()
     return render_template('hh_cases.html', cases=cases, form=form)
+
 
 @app.route('/hh/cases/<case_name_front>/<case_name_back>/', methods=['GET', 'POST'])
 def case_by_name(case_name_front=None, case_name_back=None):
@@ -72,102 +74,37 @@ def case_by_name(case_name_front=None, case_name_back=None):
             c.court_case_number = form.court_case_number.data
             db.session.add(c)
             db.session.commit()
-            flash("Case [{0}] has been updated.".format(c.case_name), "success") # TODO display which exact fields were changed and how
+            flash("Case [{0}] has been updated.".format(c.case_name),
+                  "success")  # TODO display which exact fields were changed and how
             return redirect(url_for('case_by_name', case_name_front=case_name_front, case_name_back=case_name_back))
         else:
             form = CaseForm(case)
             return render_template('specific_case.html', form=form, clients=clients, logs=logs)
 
+
 @app.route('/api/table/clients')
 def get_clients():
     draw = request.args['draw']
-    d = {
-  "draw": draw,
-  "recordsTotal": 57,
-  "recordsFiltered": 57,
-  "data": [
-    [
-      "Gloria",
-      "Little",
-      "Systems Administrator",
-      "New York",
-      "10th Apr 09",
-      "$237,500"
-    ],
-    [
-      "Haley",
-      "Kennedy",
-      "Senior Marketing Designer",
-      "London",
-      "18th Dec 12",
-      "$313,500"
-    ],
-    [
-      "Hermione",
-      "Butler",
-      "Regional Director",
-      "London",
-      "21st Mar 11",
-      "$356,250"
-    ],
-    [
-      "Herrod",
-      "Chandler",
-      "Sales Assistant",
-      "San Francisco",
-      "6th Aug 12",
-      "$137,500"
-    ],
-    [
-      "Hope",
-      "Fuentes",
-      "Secretary",
-      "San Francisco",
-      "12th Feb 10",
-      "$109,850"
-    ],
-    [
-      "Howard",
-      "Hatfield",
-      "Office Manager",
-      "San Francisco",
-      "16th Dec 08",
-      "$164,500"
-    ],
-    [
-      "Jackson",
-      "Bradshaw",
-      "Director",
-      "New York",
-      "26th Sep 08",
-      "$645,750"
-    ],
-    [
-      "Jena",
-      "Gaines",
-      "Office Manager",
-      "London",
-      "19th Dec 08",
-      "$90,560"
-    ],
-    [
-      "Jenette",
-      "Caldwell",
-      "Development Lead",
-      "New York",
-      "3rd Sep 11",
-      "$345,000"
-    ],
-    [
-      "Jennifer",
-      "Chang",
-      "Regional Director",
-      "Singapore",
-      "14th Nov 10",
-      "$357,650"
-    ]
-  ]
+    start = int(request.args['start'])
+    page_length = int(request.args['length'])
+
+    page_number = (start / page_length) + 1
+    people = Person.query.order_by('id').paginate(page_number, page_length, False).items
+    total_people_count = Person.query.count()
+
+    data = []
+    for person in people:
+        item = {}
+        item['role'] = person.role.short_name if person.role else "(no defined role)"
+        item['first_name'] = person.first_name
+        item['last_name'] = person.last_name
+        item['case_name'] = person.case.case_name if person.case else "(missing case name)"
+        data.append(item)
+
+    result = {
+        "draw": draw,
+        "recordsTotal": total_people_count,
+        "recordsFiltered": total_people_count,
+        "data": data
     }
-    return json.dumps(d)
-
-
+    return json.dumps(result)
--- app/static/js/specific_case.js
+++ app/static/js/specific_case.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("#exampleTable").DataTable({
-        "processing":true,
-        "serverSide":true,
-        "ajax":"/api/table/clients"
+        "lengthMenu": [2, 3, 5, -1],
+        "processing": true,
+        "serverSide": true,
+        "ajax": "/api/table/clients",
+        "columns": [
+            {"data": "role"},
+            {"data": "first_name"},
+            {"data": "last_name"},
+            {"data": "case_name"},
+        ]
     });
-    $("#date_opened").datepicker({ dateFormat: 'yy-mm-dd'});
-    $("#date_closed").datepicker({ dateFormat: 'yy-mm-dd'});
-    $("#editformtoggle").click(function(){
+    $("#date_opened").datepicker({dateFormat: 'yy-mm-dd'});
+    $("#date_closed").datepicker({dateFormat: 'yy-mm-dd'});
+    $("#editformtoggle").click(function () {
         var isFormDisabled = $('fieldset').prop('disabled');
         $('fieldset').prop('disabled', !isFormDisabled);
         $('#savebutton').prop('disabled', !isFormDisabled);
